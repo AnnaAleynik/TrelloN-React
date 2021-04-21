b6654ab6e3fd31d3eace4be3dca00702afafda40
AnnaAleynik <anna.mikhailovna@gmail.com>
2021-04-21 18:44:46 +0300

    add card page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import TasksPage from './components/pages/TasksPage';
+import CardPage from './components/pages/CardPage';
 import Home from './components/pages/Home';
 import routes from './config/routes';
 import TaskPage from './components/pages/TaskPage';
@@ -9,18 +9,19 @@ import CardsPage from './components/pages/CardsPage/CardsPage';
 // const initialList = [{ text: 'Позвтракать' }, { text: 'Не опаздать' }];
 
 const App = () => {
-  const { HOME, TASKS, TASK } = routes;
+  const { HOME, CARDS, CARD, TASK } = routes;
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path={HOME}>
           <Home />
         </Route>
-        <Route exact path='/cards'>
+        {/* <Route exact path='/cards'> */}
+        <Route exact path={CARDS}>
           <CardsPage />
         </Route>
-        <Route exact path={TASKS}>
-          <TasksPage />
+        <Route exact path={CARD}>
+          <CardPage />
         </Route>
         <Route path={TASK}>
           <TaskPage />
